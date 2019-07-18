03597714963135b321eb7680e59737f056120313
13522268395 <470336780@qq.com>
2019-07-18 23:16:28 +0800

    day7 topic3

---
+++ day7/DataSync.java
@@ -0,0 +1,26 @@
+package homework.day7;
+
+/**
+ * @Author myy
+ * @create 2019/7/18 21:40
+ */
+public class DataSync {
+
+/**
+ * 数据库结构同步工具的设计
+ *
+ * 给我把你的实现思路描述一下,暂时只需要文字描述就好, 若有能力的可以先行代码实现
+ *
+ * @author haoc
+ */
+
+/**
+ * 1、从information_schema库schemata表查询出所有的数据库信息，进行比对，如果不同执行创建数据库的语句
+ *
+ * 2、从information_schema库tables表中查询出每个数据库中含有的表，进行比对，执行相应的操作，创建表、删除表
+ *
+ * 3、从information_schema表columns表中查询出每个表含有的字段，进行比对，执行相应的操作，增加或删除字段
+ *
+ */
+
+}
